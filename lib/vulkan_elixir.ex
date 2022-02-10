0af6e8f6801732950fc12103d8ea0723ff9c70fc
Michał Śledź <sledzson08@gmail.com>
2022-02-10 22:13:34 +0100

    Initial commit

---
+++ lib/vulkan_elixir.ex
@@ -0,0 +1,18 @@
+defmodule VulkanElixir do
+  @moduledoc """
+  Documentation for `VulkanElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VulkanElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vulkan_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule VulkanElixirTest do
+  use ExUnit.Case
+  doctest VulkanElixir
+
+  test "greets the world" do
+    assert VulkanElixir.hello() == :world
+  end
+end
